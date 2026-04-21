94238ad0d32b7acf10d9084d9966cf1a5d191bcf
SBP80 <sbpreparaciones@gmail.com>
2026-04-21 23:12:48 +0200

    Actualizar app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const app = document.getElementById("app");
 
 app.innerHTML = `
-  <div style="max-width:500px;margin:60px auto;padding:20px;border:1px solid #ccc;border-radius:12px;text-align:center;">
+  <div style="max-width:500px;margin:60px auto;padding:20px;border:1px solid #ddd;border-radius:12px;text-align:center;font-family:Arial;">
     <h1>Zentryx</h1>
-    <p>Base JavaScript cargada</p>
+    <p>JavaScript cargado correctamente</p>
   </div>
 `;
