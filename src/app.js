c1a4d2a8f6cd1aee18af3880461a44331dc81831
SBP80 <sbpreparaciones@gmail.com>
2026-04-21 23:01:23 +0200

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const app = document.getElementById("app");
+
+app.innerHTML = `
+  <div style="max-width:500px;margin:60px auto;padding:20px;border:1px solid #ccc;border-radius:12px;text-align:center;">
+    <h1>Zentryx</h1>
+    <p>Base JavaScript cargada</p>
+  </div>
+`;
