7b00800ea7178cd09a7966b074b7c5941e5bcbe0
SBP80 <sbpreparaciones@gmail.com>
2026-05-06 20:27:21 +0200

    Actualizar app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 // ===============================
-// ZENTRYX V2615 - CORE MODULAR
+// ZENTRYX V2616 - CORE MODULAR
 // ===============================
 (function(){
   "use strict";
-  const VERSION = "2615";
+  const VERSION = "2616";
   window.ZENTRYX = window.ZENTRYX || {version:VERSION,modulos:{},estado:"base modular cargada"};
   window.ZENTRYX.version = VERSION;
   window.ZENTRYX.modulos = window.ZENTRYX.modulos || {};
