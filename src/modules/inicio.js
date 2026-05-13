a9d7e897262b28f136ece4323b015e7d73bf8920
SBP80 <sbpreparaciones@gmail.com>
2026-05-13 14:03:13 +0200

    Actualizar inicio.js

--- src/modules/inicio.js
+++ src/modules/inicio.js
@@ -1,6 +1,6 @@
 // ===============================
 // ZENTRYX PRO - INICIO
-// V2661
+// V2663
 // ===============================
 (function(){
 "use strict";
@@ -10,6 +10,7 @@ function app(){
 }
 
 window.ZENTRYX_UI_inicio=function(){
+
   if(!app()) return;
 
   app().innerHTML=`
@@ -23,27 +24,27 @@ window.ZENTRYX_UI_inicio=function(){
     <div class="zx_card">
       <h2>Acceso rápido</h2>
 
-      <button class="zx_btn_big zx_rojo" type="button" onclick="ZX_fichaje()">Fichaje</button>
-      <button class="zx_btn_big zx_azul" type="button" onclick="ZX_usuarios()">Usuarios</button>
-      <button class="zx_btn_big zx_verde" type="button" onclick="ZX_vehiculos()">Vehículos</button>
-      <button class="zx_btn_big zx_naranja" type="button" onclick="ZX_incidencias()">Incidencias</button>
-      <button class="zx_btn_big zx_morado" type="button" onclick="ZX_informes()">Informes</button>
+      <button class="zx_btn_big zx_rojo" onclick="ZX_fichaje()">Fichaje</button>
+      <button class="zx_btn_big zx_azul" onclick="ZX_usuarios()">Usuarios</button>
+      <button class="zx_btn_big zx_verde" onclick="ZX_vehiculos()">Vehículos</button>
+      <button class="zx_btn_big zx_naranja" onclick="ZX_incidencias()">Incidencias</button>
+      <button class="zx_btn_big zx_morado" onclick="ZX_informes()">Informes</button>
     </div>
 
     <div class="zx_card">
       <h2>Estado sistema</h2>
+
       <div class="zx_text">
-        <b>Versión:</b> V2661<br><br>
+        <b>Versión:</b> V2663<br><br>
+
         Diseño limpio para móvil, tablet y PC.<br><br>
-        Preparado para servidor local de empresa o servidor externo.<br><br>
+
+        Preparado para servidor local o externo.<br><br>
+
         Módulos activos: Inicio y Usuarios.
       </div>
     </div>
   `;
-};
-
-window.ZX_inicio=function(){
-  window.ZENTRYX_UI_inicio();
 };
 
 })();
